2cb77bf69b796b8cf136f504b60302f2b26d6f45
MtMercer <mateusmercer@gmail.com>
2017-09-20 22:52:49 -0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,87 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BlinkingCharacterComponent } from './blinking-character/blinking-character.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+export const routerConfig: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
+
+
+// export const routerConfig: Routes = [
+//   {
+//     path: 'home',
+//     component: HomeComponent
+//   },
+//   {
+//     path: 'about',
+//     component: AboutComponent
+//   },
+//   {
+//     path: 'courses',
+//     component: CoursesComponent
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: '**',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   }
+// ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BlinkingCharacterComponent
+    BlinkingCharacterComponent,
+    HomeComponent,
+    ProductsComponent,
+    SignUpComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
