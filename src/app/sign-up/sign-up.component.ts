24fa74fb29e689928310b36f696d971061a7aa62
MtMercer <mateusmercer@gmail.com>
2017-09-21 22:14:27 -0300

    Add signup form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent{
+  protected signupForm: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.signupForm = fb.group({
+      'first-name' : [null, Validators.compose([Validators.required, Validators.minLength(2)])],
+      'last-name' : [null, Validators.compose([Validators.required, Validators.minLength(2)])],
+      'email' : [null, Validators.compose([Validators.required, Validators.email])],
+      'password' : [null, Validators.required]
+    });
+  }
 
-  ngOnInit() {
+
+  signup(value: any) {
+    alert('What the fuck! this site does\'nt have an backend!');
   }
 
 }
