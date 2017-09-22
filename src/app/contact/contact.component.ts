814c7cb720f818595619b281ecfcb7a764e8de15
MtMercer <mateusmercer@gmail.com>
2017-09-21 23:02:29 -0300

    Add contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  protected contactForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(fb: FormBuilder) {
+    this.contactForm = fb.group({
+        'email' : [null, Validators.compose([Validators.required, Validators.email])],
+        'message' : [null, Validators.compose([Validators.required, Validators.minLength(16)])]
+    });
   }
 
+  contact(value: any) {
+    alert('What the fuck! this site does\'nt have an backend!');
+  }
 }
